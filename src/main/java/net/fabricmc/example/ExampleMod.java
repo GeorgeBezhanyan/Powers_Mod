9d8de72a4f7cf1fcb0a0726001c70b76ff739c22
GergOps <bezhanyan08@gmail.com>
2022-10-30 13:07:11 +0300

    AAA

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -5,11 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
-
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "powersmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	@Override
 	public void onInitialize() {
 
-		LOGGER.info("Hello Fabric world!");
 	}
 }
