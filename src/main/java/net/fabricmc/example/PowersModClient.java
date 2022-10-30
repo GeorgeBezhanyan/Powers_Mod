d31463bb5c5c5e3a3d2c87a926c11b3526096f9a
GergOps <bezhanyan08@gmail.com>
2022-10-30 13:18:42 +0300

    AAA

---
+++ src/main/java/net/fabricmc/example/PowersModClient.java
@@ -0,0 +1,8 @@
+package net.fabricmc.example;
+
+import net.fabricmc.api.ClientModInitilizer
+
+public class TutorialModClient implements ClientModInitilizer {
+    @Override
+    public void onInitializeClient()
+}
