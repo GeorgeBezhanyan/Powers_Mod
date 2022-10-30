9ca8377f1c475329b51cfa73d8093505dd613b20
GergOps <bezhanyan08@gmail.com>
2022-10-30 12:18:33 +0300

    Something

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 public class ExampleMod implements ModInitializer {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
-	//go
+	//gog
 	@Override
 	public void onInitialize() {
 
